b9b4fc96343988b15b95356010e90b37635b7676
Vajralavandana <vandanabv1997@gmail.com>
2022-09-13 21:03:42 +0530

    update comments and print statements file

--- class_practice/comments.py
+++ class_practice/comments.py
@@ -13,3 +13,13 @@ and 3 double quotes closed
 """
 
 print("hello")
+
+print("even number")            # printing even numbers
+
+"""
+printing
+even
+nmbers
+
+
+"""
--- print_statements.py
+++ print_statements.py
@@ -12,3 +12,11 @@ print("The value of a using format method is {}".format(a))
 b = 1
 c = 2
 print("The value of a is {}, the value of b is {} and the vale of c is {}".format(a, b, c))
+
+
+print(10)
+
+x = 8
+y = 9
+z = 11
+print("the value of x is {}, the vale of y is {} and the value of z is {}".format(x, y, z))
