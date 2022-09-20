7f886c2adbc641e6724169d1e1c30d038a5ca6f3
Vajralavandana <vandanabv1997@gmail.com>
2022-09-20 17:48:29 +0530

    update the string and list files

--- class_practice/data_type_list.py
+++ class_practice/data_type_list.py
@@ -10,7 +10,7 @@ print("type 1 declaration of empty list is {}".format(empty_lst))
 
 # another way of declaring empty list
 emp_lst = list()
-print("type 2 of declaringempty list is {}".format(emp_lst))
+print("type 2 of declaring empty list is {}".format(emp_lst))
 
 
 lst = [1, "a", "@", "10", "python@3"]
@@ -34,3 +34,12 @@ frst_ind_lst_x = lst_x[1]
 print("The value of first index of list is {}".format(frst_ind_lst_x))
 scd_ind_lst_x = lst_x[2]
 print("The value of second index of list is {}".format(scd_ind_lst_x))
+
+lst_y = [1, "python"]
+# print thon from list
+ind_lst_y= lst_y[1]
+# print(lst_y[1][2:6])
+sec_to_fif_ind_lst_y = ind_lst_y[2:6]
+print("The indexing of first element of list is {}".format(sec_to_fif_ind_lst_y))
+print("The type of zeroeth index value of list is {}".format(type(lst_y[0])))
+print("The type of first index value of list is {}".format(type(lst_y[1])))
