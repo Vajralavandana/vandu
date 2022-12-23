b19321d742846a58c296886e61b7d286f7becd5e
Vajralavandana <vandanabv1997@gmail.com>
2022-12-23 11:58:53 +0530

    Update numpy_test.py

--- class_practice/numpy_test.py
+++ class_practice/numpy_test.py
@@ -14,6 +14,10 @@ print(arr_dim[1, 1, 2])
 arr_dimm = np.array([[[1, 2, 3], [4, 5, 6]], [[2, 4, 6], [3, 5, 7]], [[7, 8, 9], [3, 2, 1]]])
 print(arr_dimm[1, 1, 2])
 print(arr_dimm[1, 1])
+
+print(arr_dimm[0, 1, 1])
 print("last element from 2nd dim:", arr_mult_dim[1, -1])
 print(arr[0:2])
+
+print(arr_mult_dim[1, -2])
 print(arr_mult_dim[0, 1:2])
